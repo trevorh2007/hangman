dfe7a25299a04f06dfa3abfc9d11779f4cfb5002
Trevor <trevorhoward2@hotmail.com>
2017-04-16 21:33:38 -0600

    Added Correct class

--- hangman.rb
+++ hangman.rb
@@ -3,8 +3,14 @@ sentence_array = starting_sentence.split("").map(&:downcase)
 accurate_count = sentence_array - [" "]
 final_sentence = starting_sentence.gsub(/[a-zA-Z]/, "_").split("")
 
+class Correct
+	def self.text
+		puts "Correct! The sentence is now: #{final_sentence.join}"
+	end
+end
+
 while sentence_array.count("") < accurate_count.count
-	puts "Guess a letter:"
+	puts "Category: Song Title \nGuess a letter:"
 	guess = gets.downcase.chomp
 	if sentence_array.include?(guess)
 		letter_index = sentence_array.find_index(guess)
