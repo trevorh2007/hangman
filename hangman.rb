37c700f5a472fde1ad04dbc8b2cdf5bdfe5e2fc3
Trevor <trevorhoward2@hotmail.com>
2017-04-16 21:05:45 -0600

    Initial commit

---
+++ hangman.rb
@@ -0,0 +1,17 @@
+starting_sentence = "Welcome to the jungle"
+sentence_array = starting_sentence.split("").map(&:downcase)
+accurate_count = sentence_array - [" "]
+final_sentence = starting_sentence.gsub(/[a-zA-Z]/, "_").split("")
+
+while sentence_array.count("") < accurate_count.count
+	puts "Guess a letter:"
+	guess = gets.downcase.chomp
+	if sentence_array.include?(guess)
+		letter_index = sentence_array.find_index(guess)
+		sentence_array[letter_index] = ""
+		final_sentence[letter_index] = guess
+		puts "Correct! The sentence is now: #{final_sentence.join}"
+	else
+		puts "Sorry, try again."
+	end
+end
