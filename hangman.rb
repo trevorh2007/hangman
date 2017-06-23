4758ae95fba2c86beaf5376a3ad0c69eb10b0f68
Trevor <trevorhoward2@hotmail.com>
2017-06-23 13:15:30 -0600

    Added logic to show all instances of guessed letter, added check if guessed letter is more than a single element

--- hangman.rb
+++ hangman.rb
@@ -8,11 +8,11 @@ else
 	puzzle = gets.chomp.downcase
 end
 puzzle_array = puzzle.split("").map(&:downcase)
-accurate_count = puzzle_array - [" "]
+accurate_count = (puzzle_array - [" "]).count
 final = puzzle.gsub(/[a-zA-Z]/, "_").split("")
 guessed_letter = []
 
-while puzzle_array.count("") < accurate_count.count
+while final.count('_') != 0
 	puts "Guess a letter:"
 	guess = gets.chomp.downcase
 	if guess.length > 1
@@ -20,19 +20,25 @@ while puzzle_array.count("") < accurate_count.count
 		sleep 1
 		puts "That's not a letter..."
 		sleep 1
-		puts "so I'm just going to use the first letter: #{guess.split('').first.upcase}"
+		puts "So I'm just going to use the first letter: #{guess.split('').first.upcase}"
 		sleep 2
 		puts
 		guess = guess.downcase.split('').first
 	end
 
-
-
 	if puzzle_array.include?(guess)
+		s = puzzle_array.join('')
+		a = (0 ... puzzle_array.length).find_all { |i| puzzle_array.join('')[i,1] == guess }.each { |i| final[i] = guess }
 		letter = puzzle_array.index(guess)
-		puzzle_array[letter] = ""
-		final[letter] = guess
-		puts "Great guess! The sentence is now: #{final.join}"
+		puzzle_array[letter] = " "
+		# binding.pry
+		if a.count > 1
+			puts "Great guess! There were #{a.count} #{guess.upcase}s!"
+			puts "Puzzle reads: #{final.join}"
+		else
+			puts "Great guess! There was a #{guess.upcase}!"
+			puts "Puzzle reads: #{final.join}"
+		end
 	else
 		if guessed_letter.include?(guess.upcase)
 			puts "You already guessed #{guess.upcase}! List of guesses: #{guessed_letter.sort.join(' ')}"
