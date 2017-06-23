d325d419e06c04886b00f14525be7472064a7c85
Trevor <trevorhoward2@hotmail.com>
2017-06-23 10:56:48 -0600

    Added logic to prevent words being guessed

--- hangman.rb
+++ hangman.rb
@@ -1,23 +1,52 @@
-starting_sentence = "Welcome to the jungle"
-sentence_array = starting_sentence.split("").map(&:downcase)
-accurate_count = sentence_array - [" "]
-final_sentence = starting_sentence.gsub(/[a-zA-Z]/, "_").split("")
+require 'pry'
 
-class Correct
-	def self.text
-		puts "Correct! The sentence is now: #{final_sentence.join}"
-	end
+puts "To do a custom puzzle type custom, otherwise hit enter for a random puzzle"
+if gets.chomp == ''
+	puzzle = "you got a friend in me"
+else
+	puts "Alright what's your custom puzzle?"
+	puzzle = gets.chomp.downcase
 end
+puzzle_array = puzzle.split("").map(&:downcase)
+accurate_count = puzzle_array - [" "]
+final = puzzle.gsub(/[a-zA-Z]/, "_").split("")
+guessed_letter = []
 
-while sentence_array.count("") < accurate_count.count
-	puts "Category: Song Title \nGuess a letter:"
-	guess = gets.downcase.chomp
-	if sentence_array.include?(guess)
-		letter_index = sentence_array.find_index(guess)
-		sentence_array[letter_index] = ""
-		final_sentence[letter_index] = guess
-		puts "Correct! The sentence is now: #{final_sentence.join}"
+while puzzle_array.count("") < accurate_count.count
+	puts "Guess a letter:"
+	guess = gets.chomp.downcase
+	if guess.length > 1
+		puts "Um..."
+		sleep 1
+		puts "That's not a letter..."
+		sleep 1
+		puts "so I'm just going to use the first letter: #{guess.split('').first.upcase}"
+		sleep 2
+		puts
+		guess = guess.downcase.split('').first
+	end
+
+
+
+	if puzzle_array.include?(guess)
+		letter = puzzle_array.index(guess)
+		puzzle_array[letter] = ""
+		final[letter] = guess
+		puts "Great guess! The sentence is now: #{final.join}"
 	else
-		puts "Sorry, try again."
+		if guessed_letter.include?(guess.upcase)
+			puts "You already guessed #{guess.upcase}! List of guesses: #{guessed_letter.sort.join(' ')}"
+		else
+			puts "Sorry, no #{guess.upcase}'s try again."
+			(guessed_letter << guess.upcase) unless guessed_letter.include?(guess.upcase)
+
+			p "Wrong Letters: #{guessed_letter.sort.join(' ')}"
+		end
 	end
 end
+
+# puts "guess"
+# guess = gets.chomp.downcase
+# guessed_letter << "h"
+# guessed_letter << "d"
+# p guessed_letter.sort.join(' ').include?(guess)
