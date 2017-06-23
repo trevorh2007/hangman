71b4c49800d1542d47b86144d7c7e01ef2915f47
Trevor <trevorhoward2@hotmail.com>
2017-06-23 14:59:47 -0600

    Added a lot of puzzle answers, added terminal table

--- hangman.rb
+++ hangman.rb
@@ -1,18 +1,37 @@
 require 'pry'
+require 'terminal-table'
+require_relative 'hangman_puzzles'
 
 puts "To do a custom puzzle type custom, otherwise hit enter for a random puzzle"
 if gets.chomp == ''
-	puzzle = "you got a friend in me"
+	puzzle = [@around_the_house.sample, @movie_quotes.sample].sample
+	if puzzle == @around_the_house.sample
+		puts "Category: Around The House"
+	else puzzle == @movie_quotes.sample
+		puts "Category: Movie Quotes"
+	end
 else
 	puts "Alright what's your custom puzzle?"
 	puzzle = gets.chomp.downcase
 end
 puzzle_array = puzzle.split("").map(&:downcase)
 accurate_count = (puzzle_array - [" "]).count
-final = puzzle.gsub(/[a-zA-Z]/, "_").split("")
+@final = puzzle.gsub(/[a-zA-Z]/, "_").split("")
 guessed_letter = []
+count = 0
 
-while final.count('_') != 0
+def puzzle_table
+	table = Terminal::Table.new do |t|
+		t.add_row []
+		t.add_row [@final.join.upcase]
+		t.add_row []
+		t.style = { :border_y => " ", :alignment => :center, :border_x => "=", :border_i => "*"}
+		t.headings = ["Puzzle"]
+	end
+	puts table
+end
+
+while @final.count('_') != 0
 	puts "Guess a letter:"
 	guess = gets.chomp.downcase
 	if guess.length > 1
@@ -27,18 +46,24 @@ while final.count('_') != 0
 	end
 
 	if puzzle_array.include?(guess)
-		s = puzzle_array.join('')
-		a = (0 ... puzzle_array.length).find_all { |i| puzzle_array.join('')[i,1] == guess }.each { |i| final[i] = guess }
+		a = (0 ... puzzle_array.length).find_all { |i| puzzle_array.join('')[i,1] == guess }.each { |i| @final[i] = guess }
 		letter = puzzle_array.index(guess)
 		puzzle_array[letter] = " "
 		# binding.pry
 		if a.count > 1
 			puts "Great guess! There were #{a.count} #{guess.upcase}s!"
-			puts "Puzzle reads: #{final.join}"
+			puzzle_table
 		else
 			puts "Great guess! There was a #{guess.upcase}!"
-			puts "Puzzle reads: #{final.join}"
+			if @final.count('_') != 0
+				puzzle_table
+			else
+				puts "Puzzle Solved!"
+				puts "It took #{count} guesses!"
+				puzzle_table
+			end
 		end
+		count += 1
 	else
 		if guessed_letter.include?(guess.upcase)
 			puts "You already guessed #{guess.upcase}! List of guesses: #{guessed_letter.sort.join(' ')}"
@@ -48,6 +73,7 @@ while final.count('_') != 0
 
 			p "Wrong Letters: #{guessed_letter.sort.join(' ')}"
 		end
+		count += 1
 	end
 end
 
